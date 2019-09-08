fe65dba7636188b6b456d37abfd191b2fe47baa9
Anwesh43 <anweshthecool0@gmail.com>
2019-09-09 03:25:37 +0530

    addnging view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedmultiarccolorscreenview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedmultiarccolorscreenview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedmultiarccolorscreenview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.multiarccolorscreenview.MultiArcColorScreenView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        MultiArcColorScreenView.create(this)
     }
 }
