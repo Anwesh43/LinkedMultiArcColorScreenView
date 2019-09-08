ab8277f9f61f016ff3ceb730d4118cf1a4c8636a
Anwesh43 <anweshthecool0@gmail.com>
2019-09-09 02:36:21 +0530

    adding constants and libraries

--- multiarccolorscreenview/src/main/java/com/anwesh/uiprojects/multiarccolorscreenview/MultiArcColorScreenView.kt
+++ multiarccolorscreenview/src/main/java/com/anwesh/uiprojects/multiarccolorscreenview/MultiArcColorScreenView.kt
@@ -4,3 +4,17 @@ package com.anwesh.uiprojects.multiarccolorscreenview
  * Created by anweshmishra on 09/09/19.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Color
+
+val colors : Array<String> = arrayOf("#9C27B0", "#2196F3", "#DD2C00", "#00C853", "#f44336")
+val circles : Int = 5
+val scGap : Float = 0.01f
+val delay : Long = 30
+val backColor : Int = Color.parseColor("#BDBDBD")
