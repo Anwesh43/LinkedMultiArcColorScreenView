6d837e83cff9931e70fa43a3330fd3ae9db773bb
Anwesh43 <anweshthecool0@gmail.com>
2019-09-09 02:46:42 +0530

    adding MultiArcColorScreenView class

--- multiarccolorscreenview/src/main/java/com/anwesh/uiprojects/multiarccolorscreenview/MultiArcColorScreenView.kt
+++ multiarccolorscreenview/src/main/java/com/anwesh/uiprojects/multiarccolorscreenview/MultiArcColorScreenView.kt
@@ -54,3 +54,20 @@ fun Canvas.drawMACSNode(i : Int, scale : Float, sc : Float, paint : Paint) {
     restore()
 }
 
+class MultiArcColorScreenView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
