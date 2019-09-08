3f5689613cfbbf824d6acf77ad8b4c50d2fa8a97
Anwesh43 <anweshthecool0@gmail.com>
2019-09-09 02:45:05 +0530

    adding Canvas extension methods

--- multiarccolorscreenview/src/main/java/com/anwesh/uiprojects/multiarccolorscreenview/MultiArcColorScreenView.kt
+++ multiarccolorscreenview/src/main/java/com/anwesh/uiprojects/multiarccolorscreenview/MultiArcColorScreenView.kt
@@ -22,3 +22,35 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawArcColorScreen(i : Int, sc1 : Float, sc2 : Float, size : Float, paint : Paint) {
+    val sc1i : Float = sc1.divideScale(i, circles)
+    val sc2i : Float = sc2.divideScale(i, circles)
+    var sweepDeg : Float = 360f * (1 - sc1.divideScale(i, circles))
+    if (sc2 > 0f) {
+        sweepDeg = 360f * sc2
+    }
+    save()
+    translate(i * 2 * size + size, 0f)
+    paint.color = Color.parseColor(colors[i])
+    drawArc(RectF(-size, -size, size, size), 360f * sc1i, sweepDeg, true, paint)
+    restore()
+}
+
+fun Canvas.drawMultiArcColorScreen(sc1 : Float, sc2 : Float, size : Float, paint : Paint) {
+    for (j in 0..(circles - 1)) {
+        drawArcColorScreen(j, sc1, sc2, size, paint)
+    }
+}
+
+fun Canvas.drawMACSNode(i : Int, scale : Float, sc : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (colors.size)
+    val size : Float = gap / 2
+    save()
+    translate(0f, h / 2)
+    drawMultiArcColorScreen(scale, sc, size, paint)
+    restore()
+}
+
